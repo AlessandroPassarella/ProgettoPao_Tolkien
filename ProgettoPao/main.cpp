04c5160618e1fc0cc4b5009cb38e5aabf1690d7e
Annie <ana_niagu@ymail.com>
2023-05-06 12:52:03 +0200

    templatizzato

--- ProgettoPao/main.cpp
+++ ProgettoPao/main.cpp
@@ -9,17 +9,22 @@ int main() {
     Entity * valar2 = new Valar("pluto", 5, 15, Ainur::Level::lower, Valar::Element::fire);
     Entity * valar3 = new Valar("topolino", 5, 15, Ainur::Level::lower, Valar::Element::air);
     
-    Qontainer f;
-    f.push( *valar1 );
-    f.push( *valar2 );
-    f.push( *valar3 );
+    Qontainer<Entity*> f;
+    f.push( valar1 );
+    f.push( valar2 );
+    f.push( valar3 );
+
+    std::cout << "@@@" << f.get(1)->getName() << '\n';
+    std::cout << "@@@" << dynamic_cast<Valar*>(f.get(1))->getName() << '\n';
     
-    Qontainer q2 = f.search(
-        [] (const Entity* x) { return (x->getName()[0] == 'p'); }
+    Qontainer<Entity*> q2 = f.search(
+        [] (Entity*const* x) {
+            return (dynamic_cast<Valar*const>(*x)->getElement() == Valar::Element::air);
+        }
     );
 
     for (unsigned i = 0; i < q2.size(); i++)
-        std::cout << q2.get(i).getName() << " begins with 'P'" << std::endl;
+        std::cout << q2.get(i)->getName() << " farts into the wind" << std::endl;
 
     return 0;
 }
--- ProgettoPao/qontainer.h
+++ ProgettoPao/qontainer.h
@@ -2,37 +2,91 @@
 #include <functional>
 #include "entity.h"
 
+template<class T>
 class Qontainer
 {
 private:
-    Entity *begin;
+    T *begin;
     unsigned int length, reserved;
 
 public:
     Qontainer();
 
-    void push(const Entity &entity);
+    Qontainer(const Qontainer &q);
+
+    void push(const T &entity);
 
     void pop(unsigned int i);
 
-    Entity& get(unsigned int i) const;
+    T& get(unsigned int i) const;
 
     unsigned size() const;
 
-    Qontainer search(std::function<bool(const Entity*)> condition) const;
-
-
-
-    /*
-    copia profonda
-    Entity* copia(){
-        Entity* arr_copy = new Entity[reserved];
-        for(int i=0; i<length; i++){
-            arr_copy[i] = begin[i];
-        }
-        return arr_copy;
-
-    }
-    */
+    Qontainer search(std::function<bool(const T*)> condition) const;
 
 };
+
+
+template<class T>
+Qontainer<T>::Qontainer() : begin(nullptr), length(0), reserved(0) {}
+
+template<class T>
+Qontainer<T>::Qontainer(const Qontainer &q) :
+begin(new T[q.length]), length(q.length), reserved(q.length) {
+    for(unsigned i = 0;i<length; i++)
+        begin[i] = q.begin[i];
+}
+
+
+template<class T>
+void Qontainer<T>::push(const T &entity){
+    if (reserved==length) {
+        //e se 0??
+        if (reserved)
+            reserved *= 2;
+        else
+            reserved = 10;
+        T* arr_copy = new T[reserved];
+        for(unsigned i=0; i<length; i++)
+            arr_copy[i] = begin[i];
+        delete[] begin;
+        begin = arr_copy;
+    }
+    begin[length] = entity; 
+    length++;
+}
+
+template<class T>
+unsigned Qontainer<T>::size() const {
+    return length;
+}
+
+template<class T>
+void Qontainer<T>::pop(unsigned int i){
+    if (i >= length)
+        throw "IndexOutOfBoundexception, can't pop neighbour's mail";
+    
+    for (unsigned j=i; j<length-1; j++)
+        begin[j] = begin[j+1];
+    
+    length--;
+}
+
+template<class T>
+T& Qontainer<T>::get(unsigned int i) const{
+    if (i < length)
+        return begin[i];
+    throw "IndexOutOfBoundexception, can't read neighbour's mail";
+}
+
+template<class T>
+Qontainer<T> Qontainer<T>::search(std::function<bool(const T*)> condition) const {
+    Qontainer<T> resultSet;
+    
+    for (unsigned int i = 0; i < length; i++) {
+        if(condition(&begin[i])) {
+            resultSet.push(begin[i]);
+        }
+    }
+    return resultSet;
+}
